149a59c6915759586efdd33a21421242e2acdd77
Bruno Garcia <brunowilliang@icloud.com>
2025-08-05 06:30:04 +0100

    deploy - 46

--- src/routes/api/checkout.ts
+++ src/routes/api/checkout.ts
@@ -5,7 +5,7 @@ import { env } from '@/lib/env';
 export const ServerRoute = createServerFileRoute('/api/checkout').methods({
 	GET: Checkout({
 		accessToken: env.POLAR_ACCESS_TOKEN_SANDBOX,
-		successUrl: '/checkout/success',
+		successUrl: 'https://www.afrodiite.com/checkout/success',
 		server: 'sandbox', // Use sandbox if you're testing Polar - omit the parameter or pass 'production' otherwise
 		theme: 'dark', // Enforces the theme - System-preferred theme will be set if left omitted
 	}),
