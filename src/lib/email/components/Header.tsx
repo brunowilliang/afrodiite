4136b7319a86411ed7722a12f87255e5cf19063e
Bruno Garcia <brunowilliang@icloud.com>
2025-10-10 15:40:42 +0100

    Remove logo.svg and favicon.ico files; update logo source in Header component to new URL

--- src/lib/email/components/Header.tsx
+++ src/lib/email/components/Header.tsx
@@ -3,7 +3,7 @@ import { Img } from '@react-email/components';
 export const Header = () => {
 	return (
 		<Img
-			src="https://www.afrodiite.com/assets/logo.svg"
+			src="https://www.afrodiite.com/logo.svg"
 			width="130"
 			height="auto"
 			alt="Afrodiite Logo"
